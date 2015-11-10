77f9612047d2af034f22ed66e51fc9f4ec8ac927
Brian Merchant <btmerchant@comcast.net>
2015-11-09 19:46:05 -0600

    Added two controllers - Todo and Pasta

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+var app = angular.module('TodoApp',[])
+
+app.controller("TodoCtrl", function($scope) {
+  $scope.title = "Welcome to your Todo App";
+})
+app.controller("PastaCtrl", function($scope) {
+  $scope.pasta = "I Love Carbs";
+  $scope.foods = "";
+})
